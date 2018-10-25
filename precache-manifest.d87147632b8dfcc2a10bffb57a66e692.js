1364e587e5a16ae618e164918a4be3502074d9c0
Olivier ESUKA <oesukam@gmail.com>
2018-10-25 11:19:29 +0200

    Updates

---
+++ precache-manifest.d87147632b8dfcc2a10bffb57a66e692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "950a4c55fc5761acab7e",
+    "url": "/static/js/main.950a4c55.chunk.js"
+  },
+  {
+    "revision": "8d86eabeb4423fd9474c",
+    "url": "/static/js/1.8d86eabe.chunk.js"
+  },
+  {
+    "revision": "950a4c55fc5761acab7e",
+    "url": "/static/css/main.581cb8f0.chunk.css"
+  },
+  {
+    "revision": "1433b61b4b2852ac35df06dbb043ec02",
+    "url": "/index.html"
+  }
+];
